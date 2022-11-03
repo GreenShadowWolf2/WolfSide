8c148656d5c2eecf6f5f99c955132e62f56d472a
Silvan Flückiger <sfl132385@iet-gibb.ch>
2022-11-03 22:00:50 +0100

    a

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -26,6 +26,7 @@ export default async function handler(req, res) {
     console.log(email);
     const mailData = {
       from: 'wolfsorganisation@gmail.com',
+      cc: `spam.flueckiger.silvan@gmail.com`,
       to: email,
       subject: `Vielen Dank für die Registrierung ${body.firstname} ${body.secondname}`,
       text: `Guten Tag ${anrede} ${body.secondname}
