65838f76d34b995948189562d933d9109316aa42
Silvan Flückiger <sfl132385@iet-gibb.ch>
2022-11-03 17:56:16 +0100

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Footer from './component/Fooder'
 import Header from './component/Header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
     return (
